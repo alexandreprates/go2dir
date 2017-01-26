8df7b70be628ae66faa9f879c31ddca108d91e42
Alexandre Prates <ajfprates@gmail.com>
2017-01-26 17:13:11 -0200

    Using bash procedure syntaxe and fixing errors on unbuntu bash

--- go2dir.sh
+++ go2dir.sh
@@ -1,14 +1,16 @@
 # GO 2 DIR
 # by Alexandre Prates <ajfprates@gmail.com>
-# 2.0 - 08-25-2016
+# 2.x - 08-25-2016
 
 FILEPATH="$HOME/.local/share/go2dir"
 FILENAME="$FILEPATH/locations.txt"
 [ ! -d $FILEPATH ] && mkdir -p $FILEPATH
 [ ! -e $FILENAME ] && touch $FILENAME
 
-go2() {
-  __not_exist() {
+function go2() {
+
+  function __not_mapped() {
+    local line
     cat $FILENAME | while read line
     do
       if [ "$1" = "$line" ]; then
@@ -18,55 +20,81 @@ go2() {
     return $?
   }
 
-  __add_dir() {
-    local dir=$(pwd)
-    local name=$(basename $dir)
+  function __add_dir() {
+    if [ -z "$1" ]; then
+      echo "You must specify the dir to add"
+      return 1
+    else
+      local dir=$(readlink -f $1)
+    fi
 
-    if __not_exist "$dir" ; then
-      echo "Adicionando $dir"
+    if [ -d "$1" ]; then
+      local name=$(basename $dir)
+    else
+      echo "Dir not exists"
+      return 1
+    fi
+
+    if __not_mapped "$dir" ; then
+      echo "Mapping $name for $dir"
       echo -e "$dir" >> $FILENAME
     else
-      echo "Dir already exist"
+      echo "$1 already mapped"
       return 1
     fi
   }
 
-  __list_dirs() {
+  function __list_dirs() {
+    local line
     echo -e "Maped dirs in $FILENAME\n"
     cat $FILENAME | sort | while read line
     do
       echo "  $line"
     done
+    echo ""
   }
 
-  __recursive_add_dir() {
+  function __recursive_add_dir() {
     find $(pwd) -maxdepth 1 -type d >> $FILENAME
     echo "Added dirs"
     find $(pwd) -maxdepth 1 -type d
   }
 
-  __show_help() {
-    echo -e "usage: go2 [options] [DIRNAME]\n\nCommand line options\n\t-a: Add the current directory to list\n\t-r add all subdirs to list\n\t-l: Lists known directories\n\t-h: Show this message\n"
+  function __show_help() {
+    echo -e "usage: go2 [options] [DIRNAME]\n\nCommand line options\n\t-a <DIRNAME>: Add the directory to list\n\t-r add all subdirs to list\n\t-l: Lists known directories\n\t-h: Show this message\n"
     return 0
   }
 
+  function  __searh_for() {
+    local line;
+    cat $FILENAME | while read line
+    do
+      name=$(basename $line)
+      if [ "$1" = "$name" ]; then
+        echo $line
+        break
+      fi
+    done
+  }
+
+  local OPTIND opt
   while getopts "alhr" opt; do
     case $opt in
       a)
-        __add_dir
-        return
+        __add_dir $2
+        return $?
         ;;
       l)
         __list_dirs
-        return
+        return 0
         ;;
       h)
         __show_help
-        return
+        return 0
         ;;
       r)
         __recursive_add_dir
-        return
+        return 0
         ;;
       \?)
         echo "Invalid option: -$OPTARG" >&2
@@ -78,18 +106,15 @@ go2() {
   if [ -z "$1" ]; then
     __show_help
     return 0
-  fi
+  else
+    local location=$(__searh_for $1)
 
-  cat $FILENAME | while read line
-  do
-    name=$(basename $line)
-    if [ "$1" = "$name" ]; then
-      echo "go to $line"
-      cd $line
-      return 0
+    if [ -z "$location" ]; then
+      echo " Sorry, i didn't find $1"
+      return 1
+    else
+      echo "go to $location"
+      cd $location
     fi
-  done
-
-  echo "$1 not found :("
-  return 1
+  fi
 }
